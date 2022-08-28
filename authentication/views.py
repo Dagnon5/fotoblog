5e9699ab16cd31f7e042abac41865f573a0bda71
Maestro <sididagnon@gmail.com>
2022-08-28 22:36:12 +0000

    Page de connexion basée sur des classes et page d'inscription

--- authentication/views.py
+++ authentication/views.py
@@ -1,12 +1,51 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
+from django.conf import settings
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
 # Create your views here.
 
 from . import forms
+from django.views.generic import View
 
+def signup_page(request):
+    form = forms.SignupForm()
+    if request.method == 'POST':
+        form = forms.SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # Auto login user
+            login(request, user)
+            return  redirect(settings.LOGIN_REDIRECT_URL)
+    return render(request, 'authentication/signup.html', context={'form':form})
+# Formulaire d'authentification personnalisé
+"""
 def logout_user(request):
     logout(request)
     return redirect('login')
+
+
+class LoginPageView(View):
+    template_name = 'authentication/login.html'
+    form_class = forms.LoginForm
+
+    def get(self, request):
+        form = self.form_class()
+        message = ''
+        return  render(request, self.template_name, context={'form':form,'message':message})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username = form.cleaned_data['username'],
+                password = form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            message = 'Identifiants invalides '
+            return render(request, self.template_name, context={'form':form,'message':message})
 
 def login_page(request):
     form = forms.LoginForm()
@@ -25,5 +64,5 @@ def login_page(request):
             else:
                 message = 'Identifiants invalides.'
     return render(request, 'authentication/login.html',
-                  context={'form': form, 'message': message})
+                  context={'form': form, 'message': message})"""
 
--- fotoblog/settings.py
+++ fotoblog/settings.py
@@ -90,17 +90,18 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'authentication.validators.ContainsLetterValidator'
     },
+
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTION':{
+            'min_length': 8,
+        }
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+        'NAME':'authentication.validators.ContainsNumberValidator'
+    }
 ]
 
 
@@ -129,3 +130,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'authentication.User'
 
 LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
